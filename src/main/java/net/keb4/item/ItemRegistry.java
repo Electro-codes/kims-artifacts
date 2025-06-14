adfec234d904b16a54c12a4ed4cdedcae76d922f
Electro-codes <electroboltz4@gmail.com>
2025-06-14 14:07:38 -0700

    Example Item

--- src/main/java/net/keb4/item/ItemRegistry.java
+++ src/main/java/net/keb4/item/ItemRegistry.java
@@ -5,7 +5,6 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistry;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ItemRegistry {
@@ -13,7 +12,7 @@ public class ItemRegistry {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
 
-    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test", () -> new Item(new Item.Properties().durability(69)));
+    public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test", FireItem::new);
 
 
     public static void register(IEventBus bus)
