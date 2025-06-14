ed1d013d810cb55ab558af4288361946407a5f11
Electro-codes <electroboltz4@gmail.com>
2025-06-14 14:18:29 -0700

    Fix hierarchy

--- src/main/java/net/keb4/kimsartifacts/event/ServerEventHandler.java
+++ src/main/java/net/keb4/kimsartifacts/event/ServerEventHandler.java
@@ -1,18 +1,18 @@
-package net.keb4.event;
+package net.keb4.kimsartifacts.event;
 
 
-import net.keb4.item.ItemRegistry;
-import net.keb4.kimsartifacts.Config;
-import net.keb4.world.ArtifactGenData;
+import net.keb4.kimsartifacts.item.ItemRegistry;
+import net.keb4.kimsartifacts.config.Config;
+import net.keb4.kimsartifacts.world.ArtifactGenData;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ServerEventHandler {
+
+
 
     @SubscribeEvent
     public static void onServerStart(ServerStartingEvent event)
